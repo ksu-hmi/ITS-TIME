dff3a4b7fd501790ba0be411a82470902f18443f
mhudso55 <47512643+mhudso55@users.noreply.github.com>
2019-05-05 20:25:19 -0400

    Add files via upload
    created input file, need to resolve time format to combine with alarm

---
+++ propract.py
@@ -0,0 +1,24 @@
+print ("Welcome to It's Time your pill reminder!")
+
+print("Enter your first_name: ")
+first_name=input()
+print("Enter your last_name: ")
+last_name=input()
+full_name=(first_name  + " " + last_name )
+
+print(full_name)
+
+print("Enter name of medication :")
+medication_name=input()
+
+print(medication_name)
+
+print("Enter directions for medication : ")
+med_direction = input()
+
+print(med_direction)
+
+print("Enter time of day medication is to be taken : ") #need input format for the time so end user will know how to enter it!
+time_take=input()
+print(time_take)
+
